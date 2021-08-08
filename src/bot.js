2c6273faabeaca6a02636022242a40e6c769ad84
csheanon1023 <87538841+csheanon1023@users.noreply.github.com>
2021-08-09 02:59:12 +0530

    Create bot.js

---
+++ src/bot.js
@@ -0,0 +1,45 @@
+require("dotenv").config();
+
+const { Client } = require('discord.js');
+
+const client = new Client({
+  partials: ['MESSAGE', 'REACTION']
+});
+
+const PREFIX = "$";
+
+client.on('ready', () => {
+  console.log(`${client.user.tag} has logged in.`);
+});
+
+client.on('messageReactionAdd', (reaction, user) => {
+  const { name } = reaction.emoji;
+  const member = reaction.message.guild.members.cache.get(user.id);
+  if (reaction.message.id === '874040719495544862') {
+    switch (name) {
+      case '1️⃣':
+        member.roles.add('873489388338810921');
+        break;
+      case '2️⃣':
+        member.roles.add('873489468466823218');
+        break;
+    }
+  }
+});
+
+client.on('messageReactionRemove', (reaction, user) => {
+  const { name } = reaction.emoji;
+  const member = reaction.message.guild.members.cache.get(user.id);
+  if (reaction.message.id === '874040719495544862') {
+    switch (name) {
+      case '🍎':
+        member.roles.remove('873489388338810921');
+        break;
+      case '🍌':
+        member.roles.remove('873489468466823218');
+        break;
+    }
+  }
+});
+
+client.login(process.env.DISCORDJS_BOT_TOKEN);
