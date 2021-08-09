37d6a9289dd46fecae13f3ae93be7c3475a4c9fc
csheanon1023 <87538841+csheanon1023@users.noreply.github.com>
2021-08-09 12:08:40 +0530

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -33,13 +33,16 @@ client.on('messageReactionAdd', (reaction, user) => {
 client.on('messageReactionRemove', (reaction, user) => {
   const { name } = reaction.emoji;
   const member = reaction.message.guild.members.cache.get(user.id);
+  console.log(`${user.username} removed reaction: ${name}`);
   if (reaction.message.id === '874040719495544862') {
     switch (name) {
       case '1️⃣':
         member.roles.remove('873489388338810921');
+        console.log(`${reaction.message.guild.roles.cache.find(r => r.id === '873489388338810921').name} role was taken away from ${user.username}`);
         break;
       case '2️⃣':
         member.roles.remove('873489468466823218');
+        console.log(`${reaction.message.guild.roles.cache.find(r => r.id === '873489468466823218').name} role was taken away from ${user.username}`);
         break;
     }
   }
