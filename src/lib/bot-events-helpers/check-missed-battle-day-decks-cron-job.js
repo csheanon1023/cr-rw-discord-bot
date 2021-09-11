d6b3335a61b9c09c672f2560c0de7cc5ad0cc100
pranjal1006 <pranjalakg@gmail.com>
2021-09-11 19:28:51 +0000

    close gitpod wrokspace snap

--- src/lib/bot-events-helpers/check-missed-battle-day-decks-cron-job.js
+++ src/lib/bot-events-helpers/check-missed-battle-day-decks-cron-job.js
@@ -6,9 +6,7 @@ const cron = require('node-cron');
 
 exports.scheduleCronsTOCollectDataAboutMissedBattleDecks = (database, client, channelList) => {
   let clanListCache = [ '#2PYUJUL', '#P9QQVJVG' ];
-  let currentRiverRaceDataCache = [];
   let isRiverRaceDataUpdatedToNextDay = false;
-  let lastInOutCronSuccessTimestamp = -1;
   let clanNameByKeyCache = {
     '#2PYUJUL': 'ROYAL WARRIORS!',
     '#P9QQVJVG': 'HARAMI_CLASHERS'
@@ -32,53 +30,55 @@ exports.scheduleCronsTOCollectDataAboutMissedBattleDecks = (database, client, ch
       return;
     }
 
-    if([3, 4, 5].includes(currentRiverRacePeriodIndex)){
-      try {
-        const currentRiverRaceDataPromises = clanListCache.map(clan => currentRiverRaceDataHelper.getCurrentRiverRaceData(clan));
-        const currentRiverRaceData = await Promise.all(currentRiverRaceDataPromises);
-        let riverRaceDataSnap = { timestamp: currentDate.getTime() }
-        currentRiverRaceData.forEach(({ data }) => {
-          if(data?.periodIndex != currentRiverRacePeriodIndex) {
-            isRiverRaceDataUpdatedToNextDay = true;
-            return;
-          }
-          riverRaceDataSnap[data?.clan?.tag?.substring(1)] = {
-            clan: data.clan,
-            periodLogs: data.periodLogs
-          };
-        });
-        databaseRepository.setLastKnownBattleDayData(riverRaceDataSnap, database);
-      } catch(e) {
-        console.error(e);
-        console.log(`${formattedCurrentTime} river race data collection failed, getCurrentRiverRaceData didn\'t get resolved`);
-        return;
-      }
+    try {
+      const currentRiverRaceDataPromises = clanListCache.map(clan => currentRiverRaceDataHelper.getCurrentRiverRaceData(clan));
+      const currentRiverRaceData = await Promise.all(currentRiverRaceDataPromises);
+      let riverRaceDataSnap = { timestamp: currentDate.getTime() }
+      currentRiverRaceData.forEach(({ data }) => {
+        if(data?.periodIndex != currentRiverRacePeriodIndex) {
+          isRiverRaceDataUpdatedToNextDay = true;
+          return;
+        }
+        riverRaceDataSnap[data?.clan?.tag?.substring(1)] = {
+          clan: data.clan,
+          periodLogs: data.periodLogs
+        };
+      });
+      databaseRepository.setLastKnownBattleDayData(riverRaceDataSnap, database);
+    } catch(e) {
+      console.error(e);
+      console.log(`${formattedCurrentTime} river race data collection failed, getCurrentRiverRaceData didn\'t get resolved`);
+      return;
     }
   });
 
-  // //CRON At each minute between 00-10 of the hours 10:00 on Sun, Mon, Fri, and Sat
-  // cron.schedule('0-10 10 * * 0,1,5,6', async () => {
-  //   const formattedCurrentTime = getCurrentTime(currentDate);
-  //   //TODO set acoording to day
-  //   let currentRiverRacePeriodIndex = 1;
-  //   if(clanListCache == null || clanListCache.length == 0) {
-  //     console.log(`${formattedCurrentTime} Skipping river race data collection as clanListCache is empty`);
-  //     return;
-  //   }
-  //   const currentRiverRaceDataPromises = clanListCache.map(clan => currentRiverRaceDataHelper.getCurrentRiverRaceData(clan));
-  //   const currentRiverRaceData = await Promise.all(currentRiverRaceDataPromises);
-  //   if(!currentRiverRaceData || currentRiverRaceData.length == 0) {
-  //     console.log('find change failed, get members data didn\'t get resolved');
-  //     return;
-  //   }
-  //   currentRiverRaceData.forEach(clanCurrentRiverRaceData => {
-  //     if(clanCurrentRiverRaceData.data.periodIndex != currentRiverRacePeriodIndex) {
-  //       //TODO handle when data is not yet updated for next day
-  //       return;
-  //     }
-  //     //TODO get DB entry for last-known-battle-day-data and calculate the unused decks and send report in discord
-  //   });
-  // });
+  //CRON At each minute between 00-10 of the hours 10:00 on Sun, Mon, Fri, and Sat
+  cron.schedule('0-10 10 * * 0,1,5,6', async () => {
+    const formattedCurrentTime = getCurrentTime(currentDate);
+    //TODO set acoording to day
+    let currentRiverRacePeriodIndex = 1;
+    if(clanListCache == null || clanListCache.length == 0) {
+      console.log(`${formattedCurrentTime} Skipping river race data collection as clanListCache is empty`);
+      return;
+    }
+    const currentRiverRaceDataPromises = clanListCache.map(clan => currentRiverRaceDataHelper.getCurrentRiverRaceData(clan));
+    const currentRiverRaceData = await Promise.all(currentRiverRaceDataPromises);
+    if(!currentRiverRaceData || currentRiverRaceData.length == 0) {
+      console.log('find change failed, get members data didn\'t get resolved');
+      return;
+    }
+
+    if([3, 4, 5].includes(currentRiverRacePeriodIndex)){
+    }
+
+    currentRiverRaceData.forEach(clanCurrentRiverRaceData => {
+      if(clanCurrentRiverRaceData.data.periodIndex != currentRiverRacePeriodIndex) {
+        //TODO handle when data is not yet updated for next day
+        return;
+      }
+      //TODO get DB entry for last-known-battle-day-data and calculate the unused decks and send report in discord
+    });
+  });
 
   
   //Helpers
@@ -92,8 +92,8 @@ exports.scheduleCronsTOCollectDataAboutMissedBattleDecks = (database, client, ch
     return datetime;
   }
 
-  const sendInOutMessage = async (change, playerTag, clan) => {
+  const sendMissedDeckReport = async (change, playerTag, clan) => {
   }
 }
 
-this.scheduleCronsTOCollectDataAboutMissedBattleDecks(databaseRepository.connectRealtimeDatabase());
+// this.scheduleCronsTOCollectDataAboutMissedBattleDecks(databaseRepository.connectRealtimeDatabase());
