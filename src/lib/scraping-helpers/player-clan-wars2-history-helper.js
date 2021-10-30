ef4fb183002675ba20f669ef1ee4861f4f17b7c5
pranjal1006 <60665945+pranjal1006@users.noreply.github.com>
2021-10-31 01:50:53 +0530

    trying github browser vs code

--- src/lib/scraping-helpers/player-clan-wars2-history-helper.js
+++ src/lib/scraping-helpers/player-clan-wars2-history-helper.js
@@ -16,7 +16,7 @@ const getPlayerClanWar2History = async (token, playerTag, playerName) => {
 		method: 'post',
 		url: `${useURL}data/player/cw2_history`,
 		headers: {
-			referer: 'https://royaleapi.com/player/G9VR9LU0',
+			referer: `${ROYALE_API_BASE_URL}player/${playerName}`,
 			'user-agent':
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36',
 			'Content-Type': 'text/plain',
@@ -25,10 +25,10 @@ const getPlayerClanWar2History = async (token, playerTag, playerName) => {
 	};
 	try {
 		const response = await axios(config);
-		// .then(function (response) {
-		//   // console.log(JSON.stringify(response.data));
-		return response.data;
-		// })
+		if (response.status == 200)
+			return response.data;
+		else
+			return false;
 	}
 	catch (error) {
 		console.log(error);
@@ -36,12 +36,12 @@ const getPlayerClanWar2History = async (token, playerTag, playerName) => {
 	}
 };
 
-module.exports = { getPlayerClanWar2History };
+module.exports = { getPlayerClanWar2HistoryOrFault };
 (async () => {
 	const token =
     'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhbGxvdyI6ImluYyIsImV4cCI6MTYzNTM3MTI2MX0.Bcte_NA1JgeKl1FfZqJmW649Pu9VF6hU3B_PFyRfmY8';
 	const playerTag = '#G9VR9LU0';
 	const playerName = 'PRANJAL';
-	const data = await getPlayerClanWar2History(token, playerTag, playerName);
+	const data = await getPlayerClanWar2HistoryOrFault(token, playerTag, playerName);
 	console.log(data);
 })();
