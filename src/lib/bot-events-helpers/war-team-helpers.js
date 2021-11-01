9cb3f140d98c5966e16de0ae4896917fe56cc6fc
pranjal1006 <pranjalakg@gmail.com>
2021-11-01 12:12:59 +0000

    support level 14 in bylevel

--- src/lib/bot-events-helpers/war-team-helpers.js
+++ src/lib/bot-events-helpers/war-team-helpers.js
@@ -15,7 +15,7 @@ exports.getMembersByLevel = async (message, args, accessLevel) => {
 	// Validations
 	if (!flag) {return message.reply('Whoops! Looks like you are not authorized to use this command.');}
 	if (args.length != 2) {return message.reply('Please specify clan code and level!');}
-	if (args[1] < 1 || args[1] > 13) {return message.reply('Level should be between 1-13!');}
+	if (args[1] < 1 || args[1] > 14) {return message.reply('Level should be between 1-14!');}
 	if (!Object.keys(clanCodes)?.includes(args[0].toLowerCase())) {return message.reply(`Please check the clan code! Valid codes are ${clanCodes.keys.join(',')}`);}
 
 	// Get and process data
@@ -68,5 +68,5 @@ const formatPlayerData = (playerData) => {
 		list.push(`${String(level).padEnd(4)}: ${cards.join(',')}`);
 	});
 
-	return `${firstLine}\n${tableHead}\n${list.join('\n')}\nOther Cards at levle ${lowerLevelCount}: ${otherCardsWithSameLevel.join(',')}`;
+	return `${firstLine}\n${tableHead}\n${list.join('\n')}\nOther Cards at levle ${lowerLevelCount}: ${otherCardsWithSameLevel.join(', ')}`;
 };
