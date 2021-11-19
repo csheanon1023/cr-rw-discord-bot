ac2e1e8d7f6307d127882b427fe542dc27865b25
pranjal1006 <pranjalakg@gmail.com>
2021-11-19 22:35:25 +0000

    add some utils

--- src/lib/utils/dateTimeUtils.js
+++ src/lib/utils/dateTimeUtils.js
@@ -1,4 +1,4 @@
-exports.getCurrentTime = (currentDate = new Date()) => {
+const getCurrentTime = (currentDate = new Date()) => {
 	const datetime = 'Last Sync: ' + currentDate.getDate() + '/'
                                + (currentDate.getMonth() + 1) + '/'
                                + currentDate.getFullYear() + ' @ '
@@ -6,4 +6,23 @@ exports.getCurrentTime = (currentDate = new Date()) => {
                                + currentDate.getMinutes() + ':'
                                + currentDate.getSeconds();
 	return datetime;
+};
+
+const convertCrTimeStampsToDateObject = (timestamp) => {
+	return isValidCrTimestamp(timestamp) ? new Date(convertCrTimeStampsToDateString(timestamp)) : false;
+};
+
+const isValidCrTimestamp = (timestamp) => {
+	return isNaN(Date.parse(convertCrTimeStampsToDateString(timestamp)));
+};
+
+const convertCrTimeStampsToDateString = (timestamp) => {
+	return `${timestamp.substring(0, 4)}-${timestamp.substring(4, 6)}-${timestamp.substring(6, 11)}:${timestamp.substring(11, 13)}:${timestamp.substring(13)}`;
+};
+
+module.exports = {
+	getCurrentTime,
+	convertCrTimeStampsToDateObject,
+	isValidCrTimestamp,
+	convertCrTimeStampsToDateString,
 };
