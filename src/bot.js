0fdac750aec132f99d533e4a098c8da88dfed7e0
pranjal1006 <60665945+pranjal1006@users.noreply.github.com>
2022-06-26 18:41:20 +0530

    move env config to new file

--- src/bot.js
+++ src/bot.js
@@ -16,6 +16,7 @@ const tempScrapeCommand = require('./lib/bot-events-helpers/temp-command-send-cw
 // Database connection
 const database = databaseRepository.connectRealtimeDatabase();
 const constants = require('./config/constants');
+const { getEnvironmentConfig } = require('./config/getEnvironmentConfig');
 
 // Init discord client
 const client = new Client({
@@ -37,104 +38,7 @@ const {
 	TEMP_CHANNEL_IDS_ARRAY,
 } = constants;
 
-let ENVIRONMENT_SPECIFIC_APPLICATION_CONFIG = {};
-switch (process.env.ENVIRONMENT_TYPE) {
-case 'production' :
-	ENVIRONMENT_SPECIFIC_APPLICATION_CONFIG = {
-		isSelfRolesEnabled: true,
-		isByLevelCommandEnabled: true,
-		isVerifyDiscordCrLinkEnabled: false,
-		isLegacyInOutLogEnabled: false,
-		isInLogEnabled: false,
-		isOutLogEnabled: false,
-		isInOutLogsComputationEnabled: true,
-		isCollectDailyRiverRaceDataEnabled: true,
-		isGenerateDailyUnusedDecksReportEnabled: true,
-		isSendActionDailyUnusedDecksReportEnabled: true,
-		isGenerateEndOfRiverRaceReportEnabled: true,
-		isSendActionEndOfRiverRaceReportEnabled: true,
-		isUpcomingChestsCommandEnabled: false,
-		isToKickListCronEnabled: false,
-		isCollectBattleDayInitialParticipantDataEnabled: false,
-		isCollectEndOfBattleDayParticipantDataEnabled: false,
-		isGenerateDailyBattleDayReportEnabled: false,
-		isSendActionDailyBattleDayReportEnabled: false,
-		isCurrentRaceConsolidatedReportCommandEnabled: false,
-		isTempScrapeCommandEnabled: true,
-	};
-	break;
-case 'staging':
-	ENVIRONMENT_SPECIFIC_APPLICATION_CONFIG = {
-		isSelfRolesEnabled: false,
-		isByLevelCommandEnabled: false,
-		isVerifyDiscordCrLinkEnabled: true,
-		isLegacyInOutLogEnabled: false,
-		isInLogEnabled: true,
-		isOutLogEnabled: true,
-		isInOutLogsComputationEnabled: true,
-		isCollectDailyRiverRaceDataEnabled: false,
-		isGenerateDailyUnusedDecksReportEnabled: false,
-		isSendActionDailyUnusedDecksReportEnabled: false,
-		isGenerateEndOfRiverRaceReportEnabled: false,
-		isSendActionEndOfRiverRaceReportEnabled: false,
-		isUpcomingChestsCommandEnabled: true,
-		isToKickListCronEnabled: false,
-		isCollectBattleDayInitialParticipantDataEnabled: true,
-		isCollectEndOfBattleDayParticipantDataEnabled: true,
-		isGenerateDailyBattleDayReportEnabled: true,
-		isSendActionDailyBattleDayReportEnabled: true,
-		isCurrentRaceConsolidatedReportCommandEnabled: true,
-		isTempScrapeCommandEnabled: false,
-	};
-	break;
-case 'dev':
-	ENVIRONMENT_SPECIFIC_APPLICATION_CONFIG = {
-		isSelfRolesEnabled: true,
-		isByLevelCommandEnabled: true,
-		isVerifyDiscordCrLinkEnabled: true,
-		isLegacyInOutLogEnabled: true,
-		isInLogEnabled: true,
-		isOutLogEnabled: true,
-		isInOutLogsComputationEnabled: true,
-		isCollectDailyRiverRaceDataEnabled: true,
-		isGenerateDailyUnusedDecksReportEnabled: true,
-		isSendActionDailyUnusedDecksReportEnabled: true,
-		isGenerateEndOfRiverRaceReportEnabled: true,
-		isSendActionEndOfRiverRaceReportEnabled: true,
-		isUpcomingChestsCommandEnabled: true,
-		isToKickListCronEnabled: true,
-		isCollectBattleDayInitialParticipantDataEnabled: true,
-		isCollectEndOfBattleDayParticipantDataEnabled: true,
-		isGenerateDailyBattleDayReportEnabled: true,
-		isSendActionDailyBattleDayReportEnabled: true,
-		isCurrentRaceConsolidatedReportCommandEnabled: true,
-		isTempScrapeCommandEnabled: false,
-	};
-	break;
-default:
-	ENVIRONMENT_SPECIFIC_APPLICATION_CONFIG = {
-		isSelfRolesEnabled: false,
-		isByLevelCommandEnabled: false,
-		isVerifyDiscordCrLinkEnabled: false,
-		isLegacyInOutLogEnabled: false,
-		isInLogEnabled: false,
-		isOutLogEnabled: false,
-		isInOutLogsComputationEnabled: false,
-		isCollectDailyRiverRaceDataEnabled: false,
-		isGenerateDailyUnusedDecksReportEnabled: false,
-		isSendActionDailyUnusedDecksReportEnabled: false,
-		isGenerateEndOfRiverRaceReportEnabled: false,
-		isSendActionEndOfRiverRaceReportEnabled: false,
-		isUpcomingChestsCommandEnabled: false,
-		isToKickListCronEnabled: false,
-		isCollectBattleDayInitialParticipantDataEnabled: false,
-		isCollectEndOfBattleDayParticipantDataEnabled: false,
-		isGenerateDailyBattleDayReportEnabled: false,
-		isSendActionDailyBattleDayReportEnabled: false,
-		isCurrentRaceConsolidatedReportCommandEnabled: false,
-		isTempScrapeCommandEnabled: false,
-	};
-}
+let ENVIRONMENT_SPECIFIC_APPLICATION_CONFIG = getEnvironmentConfig(process.env.ENVIRONMENT_TYPE || 'default');
 
 const IN_OUT_LOGS_FLAG_COLLECTION = {
 	isLegacyInOutLogEnabled: ENVIRONMENT_SPECIFIC_APPLICATION_CONFIG.isLegacyInOutLogEnabled,
